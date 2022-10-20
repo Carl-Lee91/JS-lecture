c474940c6264b8669b96d7f74b969cd2d2ee48ca
Carl-Lee91 <112924805+Carl-Lee91@users.noreply.github.com>
2022-10-20 13:39:34 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
-const player = {
-    name: "Carl",
-    points: 10, 
-    fat: true,
-};
-console.log(player);
-player.lastName = "potato";
-player.points = 15;
-console.log(player);
+const age = parseInt(prompt("나이를 말씀해 주십시오."));
+
+if (isNaN(age) || age < 0) {
+    console.log("양수를 입력해 주십시오.")
+} else if(age < 18) {
+    console.log("미성년자 이므로 음주를 할 수 없습니다.");
+} else if(age >=18 && age < 50) {
+    console.log("음주 가능 나이입니다.");
+} else if(age >=51 && age <80) {
+    console.log("음주를 자제하여 주십시오.");
+} else if (age > 80) {
+    console.log("그만 마시셈");
+} else if (age !== 100) {
+    console.log("장수하셨네요");
+} 
