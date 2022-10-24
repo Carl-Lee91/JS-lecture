da45a0759d3a9d51564002fa6b3bb2625f177b11
Carl-Lee91 <112924805+Carl-Lee91@users.noreply.github.com>
2022-10-24 10:50:58 +0900

    JS Lec 10.24

--- app.js
+++ app.js
@@ -1,15 +1,42 @@
-const age = parseInt(prompt("나이를 말씀해 주십시오."));
+const h1 = document.querySelector("div.hello:first-child h1");
 
-if (isNaN(age) || age < 0) {
-    console.log("양수를 입력해 주십시오.")
-} else if(age < 18) {
-    console.log("미성년자 이므로 음주를 할 수 없습니다.");
-} else if(age >=18 && age < 50) {
-    console.log("음주 가능 나이입니다.");
-} else if(age >=51 && age <80) {
-    console.log("음주를 자제하여 주십시오.");
-} else if (age > 80) {
-    console.log("그만 마시셈");
-} else if (age !== 100) {
-    console.log("장수하셨네요");
-} 
+
+console.dir(h1);
+
+function handleTitleClick(){
+    h1.style.color = "blue";
+}
+
+function handleMouseEnter(){
+    h1.innerText = "Mouse is here";
+}
+
+function handleMouseLeave(){
+    h1.innerText = "Mouse is gone";
+}
+
+function handleWindowResize(){
+    document.body.style.backgroundColor = "tomato";
+}
+
+function handleWindowCopy(){
+    alert("copier!");
+}
+
+function handleWindowOffline(){
+    alert("no WIFI");
+}
+
+function handleWindowOnline(){
+    alert("yes WIFI");
+}
+
+h1.addEventListener("click", handleTitleClick);
+h1.addEventListener("mouseenter", handleMouseEnter);
+h1.addEventListener("mouseleave", handleMouseLeave);
+
+
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("offline", handleWindowOffline)
+window.addEventListener("online", handleWindowOnline)
