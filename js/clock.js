cc61147f3c8757abe49ffe2fb93c3499482ce0cc
Carl-Lee91 <112924805+Carl-Lee91@users.noreply.github.com>
2022-11-06 18:16:31 +0900

    11.06 final

--- js/clock.js
+++ js/clock.js
@@ -2,10 +2,18 @@ const clock = document.querySelector("#clock")
 
 function getClock() {
     const date = new Date();
+
+    const week = ['일', '월', '화', '수', '목', '금', '토']
+    const weekday = String(week[date.getDay()]);
+
+    const years = String(date.getFullYear()).padStart(4, "0");
+    const monthes = String(date.getMonth()+1);
+    const days = String(date.getDate());
     const hours = String(date.getHours()).padStart(2, "0");
     const minutes = String(date.getMinutes()).padStart(2, "0");
     const seconds = String(date.getSeconds()).padStart(2, "0");
-    clock.innerText = `${hours}:${minutes}:${seconds}`;
+    clock.innerText = `${years}년 ${monthes}월 ${days}일 ${weekday}요일
+    ${hours}:${minutes}:${seconds}`;
 }
 
 getClock()
